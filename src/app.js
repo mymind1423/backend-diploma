4d46e247668add6faa326cbf19e40e1ec416d7a7
mymind1423 <mouadleroy@gmail.com>
2025-11-29 14:50:29 +0300

    Cors configuration

--- src/app.js
+++ src/app.js
@@ -74,10 +74,7 @@ app.use(
 const allowedOrigins = ["*"];
 
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigins);
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
